1e582ec5c2c94e29d6b0fd6921741522a123a334
taller-1915 <taller-1915@login-0.cnca>
2022-08-12 14:20:12 -0600

    Cambio al archivo tal

--- 3-matrixMultiplication/src/3-matrix-mult.c
+++ 3-matrixMultiplication/src/3-matrix-mult.c
@@ -1,8 +1,15 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define CHUNK 100
+
 void matrix_mult(double *A, double *B, double *C, int N){
 	int i, j, k;
 	register double sum;
-
+     
+    #pragma omp parallel for shared(A, B, C, N) private(i)
 	for(i = 0; i < N; i++)
+    #pragma omp parallel for schedule(dynamic, CHUNK)
 		for(j = 0; j < N; j++){
 			sum = 0;
 			for(k = 0; k < N; k++)
